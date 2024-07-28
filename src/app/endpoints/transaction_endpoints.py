423819be4bf8e6766f3ab8e73c327738dbb1fbe1
Anna <pompeeva.anna@gmail.com>
2024-07-28 19:24:58 +0000

    Create transaction service endpoints

---
+++ src/app/endpoints/transaction_endpoints.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from app.schemas.transaction_schemas import Transaction, ReportData
+import httpx
+from app.endpoints.auth_endpoints import AUTH_SERVICE_URL
+
+transaction_router = APIRouter()
+
+TRANSACTION_SERVICE_URL = 'http://127.0.0.1:8002'
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+
+@transaction_router.post("/create_transaction", status_code=status.HTTP_200_OK)
+async def create_transaction(transaction: Transaction):
+    response = httpx.get(
+        AUTH_SERVICE_URL + '/check_token' + f'?id={transaction.user_id}',
+        headers=headers,
+    )
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=response.status_code, detail=response.text)
+    else:
+        response = httpx.post(
+            TRANSACTION_SERVICE_URL + '/create_transaction',
+            data=transaction.model_dump_json(),
+            headers=headers,
+        )
+
+        if response.status_code != 200:
+            raise HTTPException(
+                status_code=response.status_code, detail=response.text)
+        return response.json()
+
+
+@transaction_router.post("/get_report", status_code=status.HTTP_200_OK)
+async def get_report(data: ReportData):
+    response = httpx.get(
+        AUTH_SERVICE_URL + '/check_token' + f'?id={data.user_id}',
+        headers=headers,
+    )
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=response.status_code, detail=response.text)
+    else:
+        response = httpx.post(
+            TRANSACTION_SERVICE_URL + '/get_report',
+            data=data.model_dump_json(),
+            headers=headers,
+        )
+
+        if response.status_code != 200:
+            raise HTTPException(
+                status_code=response.status_code, detail=response.text)
+        return response.json()
